1e913008660f84029bc7feb9fcfd94e8ad17679e
Andres Duran <andurand27@gmail.com>
2022-05-12 21:53:22 -0500

    Colocando un tipado a las peticiones http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,7 @@ export class GifsService {
   private apiKey: string = 'ytwlywMSoIsG5uUDilP78TT687TWsX6u'
   private _historial: string[] = [];
 
-  //TODO: Cambiar any por su tipo correspondiente
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -29,8 +29,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
     
-    this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=ytwlywMSoIsG5uUDilP78TT687TWsX6u&q=${query}&limit=10`)
-      .subscribe( ( resp:any ) => {
+    this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=ytwlywMSoIsG5uUDilP78TT687TWsX6u&q=${query}&limit=10`)
+      .subscribe( ( resp ) => {
         console.log(resp.data);
         this.resultados = resp.data;
       } );
